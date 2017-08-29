f44edfacf13c543120b1813b29a26efea80ebdce
Levi Bostian <levi.bostian@gmail.com>
2017-08-28 19:44:48 -0500

    Initial commit create new cocoapods lib.

---
+++ Example/Pods/Target Support Files/Pods-Moya-ErrorObjectMapper_Example/Pods-Moya-ErrorObjectMapper_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Moya_ErrorObjectMapper_Example : NSObject
+@end
+@implementation PodsDummy_Pods_Moya_ErrorObjectMapper_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-Moya-ErrorObjectMapper_Tests/Pods-Moya-ErrorObjectMapper_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Moya_ErrorObjectMapper_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Moya_ErrorObjectMapper_Tests
+@end
